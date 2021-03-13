f74446b18f3c0bfd51884d22334c8c1917796ef8
Codez Up <codezup@gmail.com>
2021-03-13 23:24:57 +0530

    Copy To Clipboard Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App() {
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./App.css";
+
+const App = () => {
+  const [copied, setCopied] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleData = (event) => {
+    setCopied(false)
+    setData(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <p>Hello World</p>
+    <div className="copy-paste-block">
+      <h2>React Copy To Clipboard Example</h2>
+      <div className="copy-block">
+        <input
+          type="text"
+          value={data}
+          onChange={handleData}
+          placeholder="Type Something to Copy"
+          required
+        />
+        <CopyToClipboard text={data} onCopy={() => setCopied(true)}>
+          <button>Click to Copy</button>
+        </CopyToClipboard>
+        {copied ? <p>Copied !</p> : null}
+      </div>
+      <div className="paste-block">
+        <input type="text" placeholder="Paste the Clipboard" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
